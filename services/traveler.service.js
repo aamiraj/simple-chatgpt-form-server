6c9879f62d52798594909504b73221cd885b8db3
Abdullah Miraj <miraj2465@gmail.com>
2023-10-26 20:55:31 +0600

    service functions updated

--- services/traveler.service.js
+++ services/traveler.service.js
@@ -3,19 +3,30 @@ const { client } = require("../utils/dbconnect");
 exports.storeTravelerDataService = async (data) => {
   const travelerData = client.db("miracleTraveler").collection("travelerData");
 
-  const { insertedId } = await travelerData.insertOne(data);
-  return insertedId;
+  try {
+    const { insertedId } = await travelerData.insertOne(data);
+    return insertedId;
+  } catch (error) {
+    return error;
+  }
 };
 
 exports.getAllTravelerDataService = async () => {
   const travelerData = client.db("miracleTraveler").collection("travelerData");
-  const allTraveler = await travelerData.find().toArray();
-  return allTraveler;
+  try {
+    const allTraveler = await travelerData.find().toArray();
+    return allTraveler;
+  } catch (error) {
+    return error;
+  }
 };
 
-exports.getSpecificTravelerService = async (queeyOption)=>{
-    const travelerData = client.db("miracleTraveler").collection("travelerData");
-    const specificTraveler = await travelerData.findOne(queeyOption)
-    return specificTraveler
-
-}
+exports.getSpecificTravelerService = async (queeyOption) => {
+  const travelerData = client.db("miracleTraveler").collection("travelerData");
+  try {
+    const specificTraveler = await travelerData.findOne(queeyOption);
+    return specificTraveler;
+  } catch (error) {
+    return error;
+  }
+};
